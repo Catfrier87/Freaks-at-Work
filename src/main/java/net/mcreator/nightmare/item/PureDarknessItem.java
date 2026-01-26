209e35fb8b1b1e31dbc31b4b8602b30ecf1ac3f9
One Sin and Hundreds of Good Deeds <catfrier87@gmail.com>
2026-01-26 02:25:37 +0100

    WIPED!!! GET THE FUCK OUT

--- src/main/java/net/mcreator/nightmare/item/PureDarknessItem.java
+++
@@ -1,9 +0,0 @@
-package net.mcreator.nightmare.item;
-
-import net.minecraft.world.item.Item;
-
-public class PureDarknessItem extends Item {
-	public PureDarknessItem(Item.Properties properties) {
-		super(properties);
-	}
-}
--- src/main/java/net/mcreator/nightmare/item/TendonItem.java
+++
@@ -1,9 +0,0 @@
-package net.mcreator.nightmare.item;
-
-import net.minecraft.world.item.Item;
-
-public class TendonItem extends Item {
-	public TendonItem(Item.Properties properties) {
-		super(properties);
-	}
-}
